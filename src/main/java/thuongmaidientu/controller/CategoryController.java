2b8a519730150ea8d4521dac0fcf9f8057b18e0b
Huy Bich <huybichc2212@gmail.com>
2022-07-06 13:43:25 +0700

    06/07 - bich them chuc nang cate controller,delete product

--- src/main/java/thuongmaidientu/controller/CategoryController.java
+++ src/main/java/thuongmaidientu/controller/CategoryController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import thuongmaidientu.model.Category;
 import thuongmaidientu.service.impl.CategoryServiceImpl;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/categories")
@@ -18,6 +20,24 @@ public class CategoryController {
     public ResponseEntity<Iterable<Category>> findAll() {
         return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
     }
-//    @PostMapping
-//    public ResponseEntity<Category> create()
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        categoryService.save(category);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Category>> update(@PathVariable Long id,@RequestBody Category category) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        if (!categoryOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        category.setId(categoryOptional.get().getId());
+        categoryService.save(category);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> delete(@PathVariable Long id) {
+        categoryService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
