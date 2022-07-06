479f08388001dfa27e20b527f2eed059214e9b02
damdat97 <dam.dat97@gmail.com>
2022-07-06 08:22:12 +0700

    first commit

---
+++ src/main/java/thuongmaidientu/CaseStudymd4Application.java
@@ -0,0 +1,13 @@
+package thuongmaidientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudymd4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudymd4Application.class, args);
+    }
+
+}
---
+++ src/test/java/thuongmaidientu/CaseStudymd4ApplicationTests.java
@@ -0,0 +1,13 @@
+package thuongmaidientu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudymd4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
